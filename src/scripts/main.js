3d5b4e07745da434b584162459552d21ad84ecbe
zmdy <hudsonuriel@gmail.com>
2018-08-30 16:19:51 -0300

    File used to main configurations

---
+++ src/scripts/main.js
@@ -0,0 +1,12 @@
+/***
+ * 
+ * ESTA FUNÇÃO É EXECUTADA QUANDO A
+ * PÁGINA TERMINAR DE CARREGAR
+ * 
+ ***/
+
+window.onload = function(){
+  // Define arquivo JSON
+	openFile("../src/config.json", "json");
+};
+
